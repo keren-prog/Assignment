080de72c6537e803f4a4f634f70b9a40d0afa2c2
keren-prog <kanimwaag@gmail.com>
2021-03-16 10:21:32 +0000

    updated assignment3

---
+++ src/AppComponent/Checkbox.js
@@ -0,0 +1,8 @@
+function Checkbox (props){
+    return (
+<div>
+    <input type={props.type}/>
+</div>
+    )
+}
+export default Checkbox;
--- src/AppComponent/Home.js
+++ src/AppComponent/Home.js
@@ -5,28 +5,37 @@ import Inputfield from './Inputfield'
 import SelectOption from './SelectOption'
 import Nav from './Nav'
 import Radio from './Radio'
+import Checkbox from './Checkbox'
 
 
 
-function Home () {
+function Home() {
 
-    return(
-    <div>
-        <Button text='Click' color='#d50000'/>
-        <Button text='Submit' color='#1e88ef'/>
-        <Button  text='Clear' color='#463e3f'/>
-        <Button text='Cancel' color=' #6AA121'/>
-        <Button text='Export' color='#00ff00'/>
-        <Button text='Alert me' color='#ffa500'/>
-        <TextArea/>
-        <Inputfield placeholder='Student id' type='text' />
-        <Inputfield placeholder='Student password' type='text'/>
-        <Inputfield placeholder='Email address' type='text'/>
-        <Nav/>
-        <Radio type='radio' value='Male' name='gender'/> Male
-        <Radio type='radio' value='Female' name='gender'/> Female
-        <SelectOption />
-    </div>
+    return (
+        <div>
+             <Button text='Click' color='#d50000' />
+            <Button text='Submit' color='#1e88ef' />
+            <Button text='Clear' color='#463e3f' />
+            <Button text='Cancel' color=' #6AA121' />
+            <Button text='Export' color='#00ff00' />
+            <Button text='Alert me' color='#ffa500' />
+            <TextArea />
+            <Inputfield placeholder='Student id' type='text' />
+            <Inputfield placeholder='Student password' type='text' />
+            <Inputfield placeholder='Email address' type='text' />
+            <Nav />
+            <Radio type='radio' value='Male' name='gender' /> Male
+            <Radio type='radio' value='Female' name='gender' /> Female
+            <SelectOption />
+
+            <div className="checkboxdiv">
+                <Checkbox type='Checkbox' /> <p>Fruits</p>
+                <Checkbox type='Checkbox' /> <p>Vegetables</p>
+                <Checkbox type='Checkbox' /> <p>Tuber</p>
+                <Checkbox type='Checkbox' /> <p>Cereals</p>
+            </div>
+        </div>
+           
     )
 }
 
